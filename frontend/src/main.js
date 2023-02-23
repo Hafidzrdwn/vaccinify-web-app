c3fb214f588b252d00d4ca44e2ff93dd15dd72f3
Hafidzrdwn <hafidz10032005@gmail.com>
2023-02-23 18:56:21 +0700

    setup frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './assets/css/bootstrap.css'
+import './assets/js/bootstrap.js'
+
+// import router from './router'
+
+createApp(App)
+  .mount('#app')
